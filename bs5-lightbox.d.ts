679902f487dd8ea76956ef488bb4213cff0fb3f9
Travis Wagner <travis@travisaw.com>
2021-09-20 04:47:43 -0400

    add type

--- bs5-lightbox.d.ts
+++ bs5-lightbox.d.ts
@@ -2,6 +2,10 @@ declare module 'bs5-lightbox' {
 	export function Lightbox(): object
 }
 
+declare module 'bs5-lightbox.js' {
+	export function Lightbox(): object
+}
+
 declare module 'bs5-lightbox/bs5-lightbox.esm' {
 	export function Lightbox(): object
 }
