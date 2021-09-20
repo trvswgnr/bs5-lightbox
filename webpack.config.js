a6b5e3928b97b9c37756f42b7f7d9c09a046d0d9
Travis Wagner <travis@travisaw.com>
2021-09-20 18:29:18 -0400

    add banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
-
+const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 module.exports = (env, {mode}) => ({
   entry: './bs5-lightbox.js',
   devtool: mode === 'development' ? 'source-map' : false,
@@ -22,6 +23,18 @@ module.exports = (env, {mode}) => ({
   resolve: {
     extensions: ['*', '.js']
   },
+  optimization: {
+    minimizer: [new TerserPlugin({ extractComments: false })],
+  },
+  plugins: [new webpack.BannerPlugin({
+	  raw: true,
+	  banner: `
+/*!
+ * Lightbox for Bootstrap 5 v${process.env.npm_package_version} (https://trvswgnr.github.io/bs5-lightbox/)
+ * Copyright ${(new Date()).getFullYear()} (https://github.com/trvswgnr/bs5-lightbox/graphs/contributors)
+ * Licensed under MIT (https://github.com/trvswgnr/bs5-lightbox/blob/main/LICENSE)
+ */`
+  })],
   output: {
 	path: path.resolve(__dirname, ''),
 	filename: mode === 'development' ? 'bs-lightbox.dev.js' : 'bs5-lightbox.min.js',
