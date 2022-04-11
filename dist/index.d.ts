de816852a85bcc4596db3de2cdc1b34f418f9648
Travis Wagner <travis@travisaw.com>
2022-04-11 14:33:43 -0400

    update types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,41 +5,42 @@
  * @module bs5-lightbox
  */
 declare class Lightbox {
-    static allowedEmbedTypes: readonly ["embed", "youtube", "vimeo", "instagram", "url"];
-    static allowedMediaTypes: readonly ["embed", "youtube", "vimeo", "instagram", "url", "image", "html"];
-    static defaultSelector: "[data-toggle=\"lightbox\"]";
-    constructor(el: string | HTMLElement, options?: any);
-    private hash;
-    private settings;
-    modalOptions: bootstrap.Modal.Options;
-    carouselOptions: bootstrap.Carousel.Options;
-    carouselElement: HTMLElement;
-    modalElement: HTMLElement;
-    modal: bootstrap.Modal;
-    carousel: bootstrap.Carousel;
-    sources: string[];
-    src: string;
-    type: Lightbox.AllowedMedia;
-    el: HTMLElement;
-    static initialize: (e: Event) => void;
-    show(): void;
-    hide(): void;
-    private setOptionsFromSettings;
-    private getSrc;
-    private getGalleryItems;
-    private getYoutubeId;
-    private getInstagramEmbed;
-    private isEmbed;
-    private createCarousel;
-    private createModal;
-    private randomHash;
+	static allowedEmbedTypes: readonly ['embed', 'youtube', 'vimeo', 'instagram', 'url'];
+	static allowedMediaTypes: readonly ['embed', 'youtube', 'vimeo', 'instagram', 'url', 'image', 'html'];
+	static defaultSelector: '[data-toggle="lightbox"]';
+	constructor(el: string | HTMLElement, options?: any);
+	private hash;
+	private settings;
+	modalOptions: bootstrap.Modal.Options;
+	carouselOptions: bootstrap.Carousel.Options;
+	carouselElement: HTMLElement;
+	modalElement: HTMLElement;
+	modal: bootstrap.Modal;
+	carousel: bootstrap.Carousel;
+	sources: string[];
+	src: string;
+	type: Lightbox.AllowedMedia;
+	el: HTMLElement;
+	static initialize: (e: Event) => void;
+	show(): void;
+	hide(): void;
+	private setOptionsFromSettings;
+	private getSrc;
+	private getGalleryItems;
+	private getYoutubeId;
+	private getInstagramEmbed;
+	private isEmbed;
+	private createCarousel;
+	private createModal;
+	private randomHash;
 }
 declare namespace Lightbox {
-    type AllowedMedia = typeof Lightbox.allowedMediaTypes[number];
-    interface Options extends bootstrap.Modal.Options, bootstrap.Carousel.Options {
-        target: string | HTMLElement;
-        gallery: string | string[];
-        size: 'default' | 'sm' | 'lg' | 'xl' | 'fullscreen';
-    }
+	type AllowedMedia = typeof Lightbox.allowedMediaTypes[number];
+	interface Options extends bootstrap.Modal.Options, bootstrap.Carousel.Options {
+		target: string | HTMLElement;
+		gallery: string | string[];
+		size: 'default' | 'sm' | 'lg' | 'xl' | 'fullscreen';
+		constrain: boolean;
+	}
 }
 export default Lightbox;
