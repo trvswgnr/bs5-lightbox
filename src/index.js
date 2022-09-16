a18b86db7d0b1ccc0e925f9b1e2dd38a118c3b2c
Travis Wagner <trvswgnr@gmail.com>
2022-09-15 23:45:48 -0400

    only use URL if caption exists

--- src/index.js
+++ src/index.js
@@ -119,12 +119,18 @@ class Lightbox {
 				const imgClasses = this.settings.constrain ? 'mw-100 mh-100 h-auto w-auto m-auto top-0 end-0 bottom-0 start-0' : 'h-100 w-100';
 				const params = new URLSearchParams(src.split('?')[1]);
 				let caption = '';
-				const url = new URL(src);
+				let url = src;
 				if (params.get('caption')) {
-					url.searchParams.delete('caption');
+					try {
+						url = new URL(src);
+						url.searchParams.delete('caption');
+						url = url.toString();
+					} catch (e) {
+						url = src;
+					}
 					caption = `<p class="lightbox-caption m-0 p-2 text-center text-white small"><em>${params.get('caption')}</em></p>`;
 				}
-				let inner = `<img src="${url.toString()}" class="d-block ${imgClasses} img-fluid" style="z-index: 1; object-fit: contain;" />`;
+				let inner = `<img src="${url}" class="d-block ${imgClasses} img-fluid" style="z-index: 1; object-fit: contain;" />`;
 				let attributes = '';
 				const instagramEmbed = this.getInstagramEmbed(src);
 				const youtubeLink = this.getYoutubeLink(src);
